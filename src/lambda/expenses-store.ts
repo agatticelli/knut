62019a747a1ffc623ba3c2aed7904789b217448e
agatticelli <agatticelli@gmail.com>
2022-03-15 05:53:47 -0300

    Add changes to expense schema + aws-sdk v3

--- src/lambda/expenses-store.ts
+++ src/lambda/expenses-store.ts
@@ -1,54 +1,43 @@
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDB, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { v4 as uuidv4 } from 'uuid';
 
 import { created, internalServerError } from '../utils/responses';
+import { DynamoExpense, StoreExpenseHandlerProps } from '../types/expenses';
 
-type StoreExpenseHandlerProps = {
-  username: string;
-  category: string;
-  description: string;
-  place: string;
-  currency: string;
-  creation_date: string;
-  payment_method: string;
-  first_impact_date: string;
-  installments: number;
-  bank: string;
-  card_issuer: string;
-  price: number;
-};
-
-const documentClient = new DynamoDB.DocumentClient();
+const dynamoClient = new DynamoDB({});
 
 // create a lambda handler to store expenses inside dynamodb
 const handler = async (event: any) => {
   const body = JSON.parse(event.body) as StoreExpenseHandlerProps;
 
   // parse the event body
-  const expenseData = {
-    id: uuidv4(),
-    username: body.username,
-    category: body.category,
-    description: body.description,
-    place: body.place,
-    currency: body.currency,
-    creation_date: new Date(body.creation_date).toISOString(),
-    payment_method: body.payment_method,
-    first_impact_date: new Date(body.first_impact_date).toISOString(),
-    installments: body.installments,
-    bank: body.bank,
-    card_issuer: body.card_issuer,
-    price: body.price,
+  const expenseId = uuidv4();
+  const expenseData: DynamoExpense = {
+    id: { S: expenseId },
+    username: { S: body.username },
+    category: { S: body.category },
+    description: { S: body.description },
+    place: { S: body.place },
+    currency: { S: body.currency },
+    creation_date: { S: new Date(body.creation_date).toISOString() },
+    payment_method: { S: body.payment_method },
+    first_impact_date: { S: new Date(body.first_impact_date).toISOString() },
+    installments: { N: body.installments.toString() },
+    bank: { S: body.bank },
+    card_issuer: { S: body.card_issuer },
+    price: { N: body.price.toFixed(2) },
   };
 
   // store the expense in dynamodb
   try {
-    await documentClient.put({
-      TableName: process.env.TABLE_NAME!,
+    const command = new PutItemCommand({
+      TableName: process.env.EXPENSES_TABLE_NAME!,
       Item: expenseData,
-    }).promise();
+    });
 
-    return created({ id: expenseData.id });
+    await dynamoClient.send(command);
+
+    return created({ id: expenseId });
   } catch (error) {
     console.error('Failed to create expense', error);
     return internalServerError();
--- src/lambda/expenses-stream-process.ts
+++ src/lambda/expenses-stream-process.ts
@@ -1,10 +1,11 @@
 import { DynamoDBStreamEvent } from 'aws-lambda';
 import { DynamoDB } from 'aws-sdk';
 
+import { createEvents } from '../utils/event-bride';
 import { noContent } from '../utils/responses';
 
 // create a lambda handler to process dynamodb stream
-const handler = async (event: DynamoDBStreamEvent) => {
+const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
   const expensesCreated = [];
   for (const record of event.Records) {
     if (record.eventName === 'INSERT') {
@@ -13,9 +14,7 @@ const handler = async (event: DynamoDBStreamEvent) => {
     }
   }
 
-  console.log(expensesCreated);
-
-  return noContent();
+  await createEvents(process.env.EVENT_BUS_NAME!, "expense:created", expensesCreated);
 };
 
 export { handler };
