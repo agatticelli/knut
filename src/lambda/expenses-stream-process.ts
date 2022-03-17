7913f8acd5ad9a37435ed012528d5513e5a4ef84
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2022-03-17 02:59:59 -0300

    Use aws-sdk v3 for dynamo unmarshall

--- src/lambda/expenses-stream-process.ts
+++ src/lambda/expenses-stream-process.ts
@@ -1,15 +1,13 @@
-import { DynamoDBStreamEvent } from 'aws-lambda';
-import { DynamoDB } from 'aws-sdk';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 import { createEvents } from '../utils/event-bride';
-import { noContent } from '../utils/responses';
 
 // create a lambda handler to process dynamodb stream
-const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
+const handler = async (event: any): Promise<void> => {
   const expensesCreated = [];
   for (const record of event.Records) {
     if (record.eventName === 'INSERT') {
-      const expense = DynamoDB.Converter.unmarshall(record.dynamodb!.NewImage!);
+      const expense = unmarshall(record.dynamodb!.NewImage!);
       expensesCreated.push(expense);
     }
   }
